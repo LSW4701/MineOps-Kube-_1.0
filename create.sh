d708bdf78c249df7a6557ffb0672a3edda147896
lsw4701 <pro_4701@naver.com>
2022-10-06 23:13:46 +0900

    df

--- create.sh
+++ create.sh
@@ -6,11 +6,22 @@
 #: Description : : Hi
 
 echo "============================================"
-echo "create MineOps EKS create script "
+echo "create MineOps EKS create script  "
+echo " module.cluster.aws_eks_cluster.this: Still creating... [10m50s elapsed] "
+echo " EKS 리소스 생성과정에서 10분 이상 시간 소모 "
 date +%Y-%m-%d
+
+# aws sts get-caller-identity 
 
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 echo $DIR
 
 cd $DIR/env/terraform-aws-ubuntu/network ; terraform init
 terraform apply -auto-approve
+
+cd $DIR/env/env/terraform-eks/3-irsa ; terraform init
+terraform apply -auto-approve
+
+aws eks update-kubeconfig --region ap-northeast-2 --name apne2-mineops --alias apne2-mineops
+# EKS연결을 위해선 ~/.kube/config 파일 내 클러스터 연결 정보를 추가해야함
+
