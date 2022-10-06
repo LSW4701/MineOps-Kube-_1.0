4b90cb8d24ecd1812f4861705b064d1d8028967f
lsw4701 <pro_4701@naver.com>
2022-10-06 17:03:32 +0900

    21

--- env/terraform-aws-ubuntu/network/main.tf
+++ env/terraform-aws-ubuntu/network/main.tf
@@ -3,6 +3,20 @@ locals {
     Project = "Network"
     Owner   = "posquit0"  ## 
   }
+
+  # private_tags= {
+  #   Project = "Network"
+  #   Owner   = "posquit0"
+  #   kubernetes.io/role/internal-elb = "1"
+    
+  # }
+
+  # public_tags= {
+  #   Project = "Network"
+  #   Owner   = "posquit0"
+  #   kubernetes.io/role/internal-elb = "1"
+    
+  # }
 }
 
 
@@ -24,6 +38,7 @@ module "vpc" {
 
   tags = local.common_tags
   
+  
 
 }
 
@@ -32,27 +47,79 @@ module "vpc" {
 # Subnet Groups
 ###################################################
 
-module "subnet_group" {
+# module "subnet_group" {
+#   source  = "tedilabs/network/aws//modules/subnet-group"
+#   version = "0.24.0"
+
+#   for_each = local.config.subnet_groups
+
+#   name                    = "${module.vpc.name}-${each.key}"
+#   vpc_id                  = module.vpc.id
+#   map_public_ip_on_launch = try(each.value.map_public_ip_on_launch, false)
+
+#   subnets = {
+#     for idx, subnet in try(each.value.subnets, []) :
+#     "${module.vpc.name}-${each.key}-${format("%03d", idx + 1)}/${regex("az[0-9]", subnet.az_id)}" => {
+#       cidr_block           = subnet.cidr
+#       availability_zone_id = subnet.az_id
+#     }
+#   }
+
+#   tags = local.common_tags
+
+# }
+
+module "subnet_group__public" {
   source  = "tedilabs/network/aws//modules/subnet-group"
   version = "0.24.0"
 
-  for_each = local.config.subnet_groups
-
-  name                    = "${module.vpc.name}-${each.key}"
+  name                    = "${module.vpc.name}-public"
   vpc_id                  = module.vpc.id
-  map_public_ip_on_launch = try(each.value.map_public_ip_on_launch, false)
+  map_public_ip_on_launch = true
 
   subnets = {
-    for idx, subnet in try(each.value.subnets, []) :
-    "${module.vpc.name}-${each.key}-${format("%03d", idx + 1)}/${regex("az[0-9]", subnet.az_id)}" => {
-      cidr_block           = subnet.cidr
-      availability_zone_id = subnet.az_id
+    "${module.vpc.name}-public-001/az1" = {
+      cidr_block           = "10.0.0.0/24"
+      availability_zone_id = "apne2-az1"
+    }
+    "${module.vpc.name}-public-002/az2" = {
+      cidr_block           = "10.0.1.0/24"
+      availability_zone_id = "apne2-az2"
     }
   }
 
-  tags = local.common_tags
-
+  tags = {
+    "kubernetes.io/role/internal-elb"                    = 1,
+ #   "kubernetes.io/cluster/${local.name_prefix}-cluster" = "shared"
+  }
 }
+
+module "subnet_group__private" {
+  source  = "tedilabs/network/aws//modules/subnet-group"
+  version = "0.24.0"
+
+  name                    = "${module.vpc.name}-private"
+  vpc_id                  = module.vpc.id
+  map_public_ip_on_launch = false
+
+  subnets = {
+    "${module.vpc.name}-private-001/az1" = {
+      cidr_block           = "10.0.10.0/24"
+      availability_zone_id = "apne2-az1"
+    }
+    "${module.vpc.name}-private-002/az2" = {
+      cidr_block           = "10.0.11.0/24"
+      availability_zone_id = "apne2-az2"
+    }
+  }
+
+  tags = {
+    "kubernetes.io/role/elb"                             = 1,
+   # "kubernetes.io/cluster/${local.name_prefix}-cluster" = "shared"
+  }
+}
+
+
 
 
 ###################################################
