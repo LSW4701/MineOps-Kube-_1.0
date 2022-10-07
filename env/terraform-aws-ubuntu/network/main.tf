ea1753465b90843ee970ce7723e4ad1e81492399
lsw4701 <pro_4701@naver.com>
2022-10-07 16:18:01 +0900

    df

--- env/terraform-aws-ubuntu/network/main.tf
+++ env/terraform-aws-ubuntu/network/main.tf
@@ -7,46 +7,6 @@ locals {
  
 }
 
-# provider "aws" {
-#   region = "ap-northeast-2"
-# }
-
-# locals {
-#   cluster_name = "apne2-mineops"
-# }
-
-# ####  https://devblog.kakaostyle.com/ko/2022-03-31-3-build-eks-cluster-with-terraform/
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-
-#   name = "apne2-mineops"
-#   cidr = "10.222.0.0/16"
-
-#   azs             = ["ap-northeast-2a", "ap-northeast-2c"]
-#   public_subnets  = ["10.222.0.0/24", "10.222.1.0/24"]
-#   private_subnets = ["10.222.2.0/24", "10.222.3.0/24"]
-
-#   enable_nat_gateway     = true
-#   one_nat_gateway_per_az = true
-
-#   enable_dns_hostnames = true
-
-#   public_subnet_tags = {
-#     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#     "kubernetes.io/role/elb"                      = "1"
-#   }
-
-#   private_subnet_tags = {
-#     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#     "kubernetes.io/role/internal-elb"             = "1"
-#   }
-# }
-
-
-###################################################
-# VPC
-###################################################
-
 module "vpc" {
   source  = "tedilabs/network/aws//modules/vpc"
   version = "0.24.0"
